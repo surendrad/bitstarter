6cf4a876219e62dece7f6e3e1006ff5f0ef677e1
Surendra Devarashetty <surendra.coursera@gmail.com>
2013-07-10 06:04:34 +0000

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
+var text = fs.readFileSync('./index.html', 'utf8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
