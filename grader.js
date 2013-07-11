964be9ba4181c0e452246491a3686c1b66939d10
Surendra Devarashetty <surendra.coursera@gmail.com>
2013-07-11 04:46:05 +0000

    grader.js uses XMLHttpRequest() call to fetch the HTML Page

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var DEFAULT_URL = null;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -44,6 +45,31 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
+
+/*
+ Uses the XMLHttpRequest for fetching the HTML Content 
+*/
+var getUrlContent = function(url, checksfile) {
+    var sys = require('util');
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var fs = require("fs");
+    var outfile = "index.html"
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+
+       if (this.readyState == 4) {
+           fs.writeFileSync(outfile, this.responseText);
+	   var checkJson = checkHtmlFile(outfile, program.checks);
+           var outJson = JSON.stringify(checkJson, null, 4);
+           console.log(outJson);
+       }
+    };
+
+    xhr.open("GET", url, true);
+    xhr.send();
+};
+
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
@@ -55,6 +81,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile) {
+    $ = getUrlContent(url);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+}; 
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +102,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL of the website', DEFAULT_URL)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.url == null)  {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    } else {
+        console.log("URL is "+program.url);
+        getUrlContent(program.url);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
