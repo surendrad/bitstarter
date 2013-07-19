b58376218b9f26dcedd8ec7c230beffe8c7e3e12
Surendra Devarashetty <surendra.devarashetty@gmail.com>
2013-07-19 08:04:05 +0000

    Homework 4 Part 2 - First Attempt

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(text.toString('utf8'));
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
